5f65053887a3f830a1be9593308bdaacc1d11c26
PythonCodeNemesis <durgaminakshmi@gmail.com>
2023-02-15 21:01:35 +0530

    update flask app

--- app1.py
+++ app1.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS, cross_origin
 import vonage
 from flask_apscheduler import APScheduler
+import random
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -15,21 +16,45 @@ client = Client("SID", "TOKEN")
 # A list to store phone numbers and frequency of reminders
 subscribers = []
 
+# list of strings
+eco_reminders = [
+    "Compost food scraps and yard waste to reduce landfill waste",
+    "Use reusable water bottles and coffee cups",
+    "Switch to LED light bulbs to save energy",
+    "Buy local and in-season produce to reduce carbon emissions from transportation",
+    "Take public transportation or walk instead of driving whenever possible",
+    "Fix leaky faucets to conserve water",
+    "Donate or repurpose unwanted items instead of throwing them away",
+    "Choose environmentally-friendly cleaning products",
+    "Reduce meat consumption to reduce the environmental impact of livestock farming",
+    "Use a clothesline to dry clothes instead of a dryer",
+    "Take steps to reduce energy consumption in your home, such as adding insulation",
+    "Avoid single-use plastics such as straws, utensils, and packaging",
+    "Plant a vegetable garden to grow your own food",
+    "Shop at secondhand stores to reduce the environmental impact of fast fashion",
+    "Turn off the tap while brushing your teeth to conserve water",
+    "Use a reusable razor or electric shaver instead of disposable razors",
+    "Choose to walk or bike for short trips instead of using a car",
+    "Use natural lighting whenever possible instead of turning on lights",
+    "Buy products with minimal packaging to reduce waste",
+    "Take public transportation, carpool, or work from home to reduce your carbon footprint"
+]
+
+
 def send_reminders(phone_number):
   
-  responseData = sms.send_message(
-  {
-      "from": "Vonage APIs",
-      "to": phone_number,
-      "text": "Drink a glass of water now!",
-  }
+  message = client.messages.create(
+    to=phone_number,
+    from_="YOUR TWILIO NUMBER",
+    body=random.choice(eco_reminders)
   )
+  print(message["messages"])
+  print(message["messages"][0])
 
-
-  if responseData["messages"][0]["status"] == "0":
+  if message["messages"][0]["status"] == "0":
       print("Message sent successfully.")
   else:
-      print(f"Message failed with error: {responseData['messages'][0]['error-text']}")
+      print(f"Message failed with error: {message['messages'][0]['error-text']}")
 
 @app.route("/sign-up", methods=["POST"])
 @cross_origin()
@@ -38,6 +63,7 @@ def sign_up():
   phone_number = data["phoneNumber"]
   frequency = data["frequency"]
   print(phone_number)
+  
 
   # Add the phone number and frequency to the subscribers list
   subscribers.append({"phone_number": phone_number, "frequency": frequency})
